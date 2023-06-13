2d2502f45fc274d372735241e80d23a9af3d7e17
HassanAbbas7 <129033939+HassanAbbas7@users.noreply.github.com>
2023-06-13 13:28:57 +0500

    Update index.jsx
    fixed getToken

--- src/auth/index.jsx
+++ src/auth/index.jsx
@@ -22,7 +22,7 @@ export const setToken = (token) => {
 
 
 const getNewToken = async () => {
-    if (localStorage.getItem("accessToken").length  < 1) {return;}
+    if (localStorage.getItem("accessToken") == null) {return}
     try {
         const res = await axios.post(REFRESH_TOKEN_URL, {
                "refresh": localStorage.getItem("refreshToken")
